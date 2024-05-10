d7af6036412effc3aac3154ac6c9e7dad9386f53
koukaa_500 <koukinabil73@gmail.com>
2024-05-10 17:41:10 +0100

    chronometre et score

--- src/app/components/exercices/exercice3/exercice3.component.ts
+++ src/app/components/exercices/exercice3/exercice3.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./exercice3.component.css']
 })
 export class Exercice3Component {
+  progress: number = 0;
+  progressWidth?: number;
+  timer=60;
+  listSyllabes: any =['a', 'ma', 'am', 'al', 'mi', 'li', 'mil', 'imi', 'lam', 'lal', 'lil', 'lim', 'mil', 'lam', 'am', 'imi', 'ili', 'ima', 'lima', 'alal', 'mami', 'mimi', 'ali', 'la', 'ma', 'il', 'ami', 'mal', 'amal', 'ala']
+  syllabesGroups: string[][] = [];
+  buttonValue:string = "Commencer"
+  score : number = 0;
+  
+constructor() { }
+
+ngOnInit(): void {
+  this.splitSyllabes();
+  this.updateProgress();
+}
+
+updateProgress() {
+  setTimeout(() => {
+    this.progress += 10; // Increase progress by 10%
+    this.progressWidth = this.progress; // Set progressWidth to match progress
+    if (this.progress <= 100) {
+      this.updateProgress(); // Call the function recursively until progress reaches 100%
+    }
+  }, 1000); // Update progress every 1 second
+}
+
+splitSyllabes() {
+  for (let i = 0; i < this.listSyllabes.length; i += 5) {
+    this.syllabesGroups.push(this.listSyllabes.slice(i, i + 5));
+  }
+}
+hideCTADiv() {
+  this.buttonValue = "Réjouer";
+}
 
 }
